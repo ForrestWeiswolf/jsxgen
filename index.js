e3ca8e98b572994cbab9635f2191436848d9a971
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2019-02-15 11:26:23 -0500

    Command line flag to create stateful component

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const fs = require('fs')
 const yargs = require('yargs')
-  .usage('Usage: jsxgen ComponentName')
+  .usage('Usage: $0 <ComponentName> [options]')
   .demandCommand(1)
+  .boolean('stateful')
+  .alias('stateful', 's')
+  .describe('s', 'Make a stateful component')
+
 const createComponent = require('./createComponent')
 
 const argv = require('yargs').argv
 
 argv['_'].forEach(componentName => {
-  fs.writeFile(`${componentName}.jsx`, createComponent(componentName), function (err) {
+  const componentText = createComponent(componentName, argv['stateful'])
+
+  fs.writeFile(`${componentName}.jsx`, componentText, function (err) {
     if (err) {
       console.log(err)
     } else {
