163905a1ed2499e6daabb499faa066f824b38717
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2019-02-15 11:25:28 -0500

    Can create functional components

--- createComponent.js
+++ createComponent.js
@@ -1,22 +1,34 @@
-function createComponent(name) {
+function createComponent(name, isStateful) {
   const imports = (
-    `import React, { Component } from 'react'\n` +
+    `import React${isStateful ? ', { Component }' : ''} from 'react'\n` +
     `import PropTypes from 'prop-types'\n\n`
   )
 
-  const component = (
-    `class ${name} extends Component {\n` +
-    `  constructor(props) {\n` +
-    `    super(props)\n` +
-    `    this.state = {}\n` +
-    ` }\n\n` +
-    ` render(){\n` +
-    `   return (\n` +
-    `     <div></div>\n` +
-    `   )\n` +
-    ` }\n` +
-    `}\n\n`
-  )
+  let component
+  if (isStateful) {
+    component = (
+      `class ${name} extends Component {\n` +
+      `  constructor(props) {\n` +
+      `    super(props)\n` +
+      `    this.state = {}\n` +
+      ` }\n\n` +
+      ` render(){\n` +
+      `   return (\n` +
+      `     <div></div>\n` +
+      `   )\n` +
+      ` }\n` +
+      `}\n\n`
+    )
+  } else {
+    component = (
+      `const ${name} = props => {\n` +
+      `  return (\n` +
+      `    <div>\n` +
+      `    </div>\n` +
+      `  )\n` +
+      `}\n\n`
+    )
+  }
 
   const propTypes = (
     `${name}.propTypes = {\n` +
