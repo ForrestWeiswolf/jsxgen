943142b6128df4f5be4e873659f0d3c9b9230f21
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2019-03-10 14:31:45 -0400

    Revert "Try using process.stdin instead of rl"
    This reverts commit 92c279745cac35d8b7cbe738f71ef0c3fcf19bc3.

--- index.js
+++ index.js
@@ -21,31 +21,23 @@ const createComponent = require('./createComponent')
 
 const argv = yargs.argv
 
-// const rl = require('readline').createInterface({
-//   input: process.stdin,
-//   output: process.stdout
-// })
-
-var standardInput = process.stdin
-
-// Set input character encoding.
-standardInput.setEncoding('utf-8')
-
+const rl = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
 
 function confirmYN(message, yesCallback, noCallback) {
-  console.log({message})
-  standardInput.on('data', (answer) => {
-    // console.log(answer)
+  rl.question(message, (answer) => {
     if (answer[0] && answer[0].toLowerCase() === 'y') {
-      // console.log('close')
-      // rl.close()
+      console.log('close')
+      rl.close()
       yesCallback()
     } else {
-      // console.log('close')
-      // rl.close()
+      console.log('close')
+      rl.close()
       noCallback()
     }
-  })
+  });
 }
 
 function createFile(path, text, next, flag = 'wx') {
@@ -59,7 +51,7 @@ function createFile(path, text, next, flag = 'wx') {
       confirmYN(
         `${path}.jsx already exists! Overwrite?\n`,
         () => {
-          createFile(path, text, next, 'w')
+          // createFile(path, text, next, 'w')
           next()
         },
         () => {
@@ -77,26 +69,21 @@ function createFile(path, text, next, flag = 'wx') {
 }
 
 console.log(argv['_'])
-argv['_'].forEach(namepath => {
-  const namepathArr = namepath.split('/')
-  const componentName = namepathArr[namepathArr.length - 1]
+// argv['_'].forEach(namepath => {
+//   const namepathArr = namepath.split('/')
+//   const componentName = namepathArr[namepathArr.length - 1]
 
-  const componentText = createComponent(
-    componentName,
-    argv
-  )
+//   const componentText = createComponent(
+//     componentName,
+//     argv
+//   )
 
-  console.log(namepath)
-  createFile(namepath, componentText)
-})
+//   console.log(namepath)
+//   createFile(namepath, componentText)
+// })
 
-function recurseThrough(arr, callback, end, index = 0) {
-  if (index < arr.length) {
-    console.log(index)
-    callback(arr[index], () => recurseThrough(arr, callback, end, index + 1))
-  } else {
-    end()
-  }
+function recurseThrough(arr, callback, index = 0) {
+  callback(arr[index], () => recurseThrough(arr, callback, index + 1))
 }
 
 recurseThrough(argv['_'], (namepath, next) => {
@@ -110,5 +97,4 @@ recurseThrough(argv['_'], (namepath, next) => {
 
   console.log(namepath)
   createFile(namepath, componentText, next)
-}, process.exit)
-
+})
